d8ffb8dbe3572f7f0605ca91ebf1f8db148ae411
ericalab <erika.yyma@gmail.com>
2020-04-27 21:32:51 +0900

    seedで複数タグを作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,4 +9,15 @@ if Rails.env == 'development'
     (1..50).each do |i|
         Board.create(name: "ユーザー#{i}", title: "タイトル#{i}", body: "本文#{i}")
     end
+
+    Tag.create!([
+        { name: 'Ruby"'}
+        { name: 'Python'} 
+        { name: 'PHP'}
+        { name: 'Javascript'}
+        { name: '勉強会'}
+        { name: 'セミナー'}
+        { name: 'その他'}
+   ])
+    end 
 end
